687623507131b760a57dcdb8767cafbd929c1e5b
Nurtal <nathan.foulquier.pro@gmail.com>
2016-05-19 16:31:47 +0200

    scraping exemple

--- trashlib.py
+++ trashlib.py
@@ -8,8 +8,8 @@ for the Murlock Project
 import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
-
-
+import requests
+from lxml import html
 
 def sendMailWrapper(fromaddr, toaddr, subject, content, passwd): 
  
@@ -31,4 +31,12 @@ def sendMailWrapper(fromaddr, toaddr, subject, content, passwd):
 
 
 
+page = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
+tree = html.fromstring(page.content)
+#This will create a list of buyers:
+buyers = tree.xpath('//div[@title="buyer-name"]/text()')
+#This will create a list of prices
+prices = tree.xpath('//span[@class="item-price"]/text()')
+print 'Buyers: ', buyers
+print 'Prices: ', prices
 
