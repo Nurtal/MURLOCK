8e94d8ed26962748028aea6adb412a105fb8ced1
Nurtal <nathan.foulquier.pro@gmail.com>
2016-05-20 16:33:16 +0200

    scrape code from stackoverflow

--- trashlib.py
+++ trashlib.py
@@ -31,12 +31,53 @@ def sendMailWrapper(fromaddr, toaddr, subject, content, passwd):
 
 
 
+
+def collectCodeFromSOF(url):
+	"""
+	=> Scan a web page looking for html code
+	balise.
+	=> Return Balise content in a string array
+	
+	----------------
+	Optimize for StackOverFlow (SOF) : IN PROGRESS
+
+	"""
+
+	page = requests.get(url)
+	tree = html.fromstring(page.content)
+	code = tree.xpath('//code/text()')
+
+	return code
+
+
+
+
+
+"""
+Test Space
+"""
+
+
+#stuff = collectCodeFromSOF('http://stackoverflow.com/questions/846257/how-can-i-remove-the-last-seven-characters-of-a-hash-value-in-perl')
+#print stuff
+"""
+# Example
 page = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
 tree = html.fromstring(page.content)
 #This will create a list of buyers:
 buyers = tree.xpath('//div[@title="buyer-name"]/text()')
 #This will create a list of prices
 prices = tree.xpath('//span[@class="item-price"]/text()')
-print 'Buyers: ', buyers
-print 'Prices: ', prices
+#print 'Buyers: ', buyers
+#print 'Prices: ', prices
+"""
+
+
+#page = requests.get('http://stackoverflow.com/questions/846257/how-can-i-remove-the-last-seven-characters-of-a-hash-value-in-perl')
+#tree = html.fromstring(page.content)
+#code = tree.xpath('//code/text()')
+#print code;
+
+
+
 
